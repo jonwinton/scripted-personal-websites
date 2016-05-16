4ded5efeb3cb3bbbd0091f4b0809dc8e311a43d5
Jon Winton <winton.jon@gmail.com>
2016-05-15 22:45:34 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function($) {
+
+  var $menuBtn = $('.menuBtn');
+  var $menu = $('.menuContainer');
+
+  $menuBtn.on('click', function() {
+    $menu.fadeToggle(300);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(window.jQuery);
